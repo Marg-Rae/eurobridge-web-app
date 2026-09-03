4ab8e1337ab83030df3239077f8013ae2bb676f4
Marg-Rae <margaretchepkoech01@gmail.com>
2026-09-03 08:45:03 +0300

    changes footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
       <div className="footer-contact">
         <span className="footer-title">Visit us</span>
         <p>Email: info@eurobridgelanguageinstitute.com</p>
-        <p>Phone/WhatsApp: +254722108799</p>
+        <p>Phone/WhatsApp: 0724677235</p>
         <p>
           Location: Second Floor, Champion Heights Building, Behind Comfy Hotel,
           Eldoret(K)
@@ -60,7 +60,7 @@ const Footer = () => {
               <path d="M13.5 9.3V7.6c0-.7.5-1 1.1-1h1.7V4h-2.4c-2.1 0-3.4 1.4-3.4 3.5v1.8H8.4v2.6h2.1V20h2.9v-8.1h2.4l.4-2.6h-2.8z" />
             </svg>
           </a>
-          <a className="social whatsapp" href="https://wa.me/254722108799" aria-label="WhatsApp">
+          <a className="social whatsapp" href="https://wa.me/254724677235" aria-label="WhatsApp">
             <svg viewBox="0 0 24 24" aria-hidden="true">
               <path d="M12 4.4a7.6 7.6 0 0 0-6.6 11.2L4 20l4.6-1.3a7.6 7.6 0 1 0 3.4-14.3zm0 13.8a6.2 6.2 0 0 1-3.2-.9l-.2-.1-2.7.8.8-2.6-.2-.2a6.2 6.2 0 1 1 5.5 3z" />
               <path d="M15.7 13.9c-.2-.1-1.2-.6-1.4-.7-.2-.1-.3-.1-.4.1-.1.2-.5.7-.6.8-.1.1-.2.1-.4 0-.2-.1-.8-.3-1.5-.9-.6-.5-1-1.2-1.1-1.4-.1-.2 0-.3.1-.4l.3-.3c.1-.1.1-.2.2-.3.1-.1.1-.2 0-.4-.1-.1-.4-1-.6-1.3-.2-.3-.3-.3-.4-.3h-.4c-.1 0-.3.1-.4.2-.2.2-.6.6-.6 1.5s.7 1.7.8 1.8c.1.1 1.4 2.2 3.4 3.1.5.2.9.4 1.2.5.5.2.9.2 1.2.1.4-.1 1.2-.5 1.4-1 .2-.5.2-.9.1-1-.1-.1-.2-.2-.4-.3z" />
